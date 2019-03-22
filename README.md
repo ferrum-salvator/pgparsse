c29ad2584eb3a3a6736ae3e0323613963c049866
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2019-03-22 19:36:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+The simplest way to make use of the package is to add the directory to $Path:
+```
+AppendTo[$Path, FileNameJoin[NotebookDirectory[],"pgparsse"]]
+Needs["pgparsse`"]
+```
---
+++ pgparsse.m
@@ -0,0 +1,31 @@
+BeginPackage["pgparsse`"]
+Unprotect @@ Names["pgparsse`*"];
+ClearAll @@ Names["pgparsse`*"];
+
+ParsePGPass::usage = "Parse .pgpass"
+
+Begin["`Private`"]
+
+pgpassFile[path_] := Import[path];
+
+pgpassFormattedFile[path_] := If[
+    ListQ[pgpassFile[path]],
+    pgpassFile[path],
+    {{pgpassFile[path]}}
+];
+
+pgpassParsedFile[path_] := Map[
+    StringSplit[#, ":"][[1]] &,
+    pgpassFormattedFile[path]
+];
+
+parseLine[l_] := <|
+    "JDBC String" ->
+            "jdbc:postgresql://" <> l[[1]] <> ":" <> l[[2]] <> "/" <> l[[3]],
+    "Username" -> l[[4]], "Password" -> l[[5]]|>;
+
+ParsePGPass[path_ : "~/.pgpass"] := Map[parseLine, pgpassParsedFile[path]];
+
+End[]
+Protect @@ Names["pgparsse`*"];
+EndPackage[]
